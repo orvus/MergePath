e0e88b3ea185a8e67ef4a03ce7d8203ddff6f3d4
Orvus <aurelien_du_77380@hotmail.fr>
2017-12-07 07:02:34 +0100

    fin tri (opti a faire sur n^2)

--- mergepath.cu
+++ mergepath.cu
@@ -2,9 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define NB 2
-#define NTPB 4 
-#define sizeTab
+#define NB 1
+#define NTPB 2
+
+#define SIZE 32
+
+
 void testCUDA(cudaError_t error, const char *file, int line)  {
    if (error != cudaSuccess) {
       printf("There is an error in file %s at line %d : %d\n", file, line, error);
@@ -29,19 +32,28 @@ __device__ void printRange(int idx,int* t, int d,int f){
       printf("]\n");   
    }
 
-      // printf("tab[%d:%d]\", d,f);
    
 }
 
 
-__device__ void merge(int idx,int* C,int* A, int ab,int ad,int* B, int bb, int bd, int pos,int size){
+/*------ @param : int 
+         idx   : indice du thread qui entre dans la fonction
+         C     : tableau de résultat 
+         A     : tableau d'entrée
+         ab    : indice de debut pour A  
+         ad    : indice de fin pour A
+         B     : tableau d'entrée
+         bb    : indice de début pour B
+         bd    : indide de fin pour B
+
+
+*/
+__device__ void merge(int idx,int* C,int* A, int ab,int ad,int* B, int bb, int bd){
    int start = ab+bb;
    int i = 0;
-  // printf("->%d\n", ad-ab + bd-bb);
    while(1){
       if(ab >= ad && bb >= bd){
 
-        // printRange(idx,C,0,16);
          return;
       }else if(ab == ad){
          C[start+i] = B[bb];
@@ -61,26 +73,23 @@ __device__ void merge(int idx,int* C,int* A, int ab,int ad,int* B, int bb, int b
 }
 
 
+/*------ @param : 
+            C : fusion de A et B trié
+            A : tableau d' entrée trié de lenR éléments
+            B : tableau d' entrée trié de lenR éléments
+            len* : longeur du talbeau *
+            nbT : nombre de thread dans la fonction 
 
+*/
 __device__ void mergePath(int* C,int* A,int lenA,int *B,int lenB,int nbT){
    
    int idx = (threadIdx.x + blockIdx.x * blockDim.x)% nbT +1;
 
    if(idx <= nbT){
-
-      //C[idx-1] = idx;
-      // Ad[0] = 0;
-      // Bd[0] = 0;
-      int test = 0;
-      printf("\t\t%d  = idx (%d , %d et %d , %d\n",idx,A[0] ,A[1] , B[0], B[1]   );
-      if(A[0] == 2 && A[1] == 9 && lenA == 2 && B[0] == 7 && B[1] == 10 ){
-         test =1;
-         printf("!!!!!!!!!!! %d\n",idx);
-      }
+ 
       Ad[idx-1] = 0;
       Bd[idx-1] = 0;
       if(idx == 1){
-      //    // printf("\n");
          Ad[nbT] = lenA;
          Bd[nbT] = lenB;
        
@@ -92,23 +101,18 @@ __device__ void mergePath(int* C,int* A,int lenA,int *B,int lenB,int nbT){
       int atop = (index > lenA)? lenA-1: index;
       int btop = (index > lenA)?  index - (lenA-1) : 0;
       int abot = btop;
-      //int i = 0;
       int offset;
       int ai;
       int bi;
       int flag = 0;
       while(!flag && idx != nbT){
-         //printf("%d\n",i++ );
          offset = (int)floor(((float)(atop - abot))/(float)(2));
          ai = atop - offset;
          bi = btop + offset;
-         if(test){
-            printf("%d : (%d,%d) off: %d, atop %d btop %d abot %d\n %d > %d et %d <= %d ?  \n",idx,ai,bi,offset,atop,btop,abot,A[ai], B[bi-1] ,A[ai-1],B[bi]);
-         }
+        
          if( bi == 0 || ai >= lenA || A[ai] > B[bi-1]){
             if(A[ai-1] <= B[bi])
             {
-              printf("%d : (%d,%d)\n",idx,ai,bi );
                Ad[idx] = ai;
                Bd[idx] = bi;
                flag = 1;
@@ -123,79 +127,114 @@ __device__ void mergePath(int* C,int* A,int lenA,int *B,int lenB,int nbT){
          }
       }
       __syncthreads();
-         if(/*idx == 1 && */test){
-            printf("plop\n");
-         printRange(1,Ad,0,9);
-         printRange(1,Bd,0,9);
-      }
-      int size = (lenA+lenB)/(nbT);
+     // int size = (lenA+lenB)/(nbT);
       for (int i = 1; i <= nbT; ++i)
       {
-         merge(idx,C,A,Ad[i-1],Ad[i],B,Bd[i-1],Bd[i],i-1,size);
-        // printRange(0,C,0,16);
+         merge(idx,C,A,Ad[i-1],Ad[i],B,Bd[i-1],Bd[i]);
       }
    }
 }
 
-__global__ void sort(int *R,int* S,int* T,int lenR){
+
+__device__ void mergeTab(int* C,int* A,int a,int* B,int b)
+{
+   int start = 0;
+   int i = 0;
+   int a_ = 0,b_ = 0;
+   while(1){
+      if(a_ >= a && b_ >= b){
+         return;
+      }else if(a_ == a){
+         C[start+i] = B[b_];
+         b_++;
+      }else if(b_ == b){
+         C[start+i] = A[a_];
+         a_++;
+      }else if( A[a_] > B[b_]){
+         C[start+i] = B[b_];
+         b_ += 1; 
+      }else{
+         C[start+i] = A[a_];
+         a_++;
+      }
+      i++;
+   }
+}
+
+
+/*------ @param : 
+            R : tableau de sortie trié de lenR éléments
+            S : tableau d' entrée trié de lenR éléments
+            lenR : longeur des tableaux
+
+*/
+__global__ void sort(int *R,int* S,int lenR){
    int idx = threadIdx.x + blockIdx.x * blockDim.x;
    int size = lenR/(NB*NTPB);
    int tmp;
    int turn = 1;
- //  printf("debut\n");
-   if(2*lenR < NB*NTPB){
-   //   printf("inf\n");
-   }else{
-     // printf("sup %d\n",size);
-
-      for (int j = 0; j < size; j++)
+   if(lenR < 2*NB*NTPB){
+      size = 2;
+   }
+   printf("%d : size\n",size );
+   //création de tableau trié de "size" éléments
+   for (int j = 0; j < size; j++)
+   {
+      for (int k = j; k < size; k++)
       {
-         for (int k = j; k < size; k++)
-         {
-            if( R[j+idx*size] > R[k+idx*size]){
-               tmp = R[j+idx*size];
-               R[j+idx*size] = R[k+idx*size];
-               R[k+idx*size] = tmp;
-            }
+         if( R[j+idx*size] > R[k+idx*size]){
+            tmp = R[j+idx*size];
+            R[j+idx*size] = R[k+idx*size];
+            R[k+idx*size] = tmp;
          }
-      }
-      for(int i = 0; i < size ; i++){
-         S[idx*size+i] = R[idx*size+i];
-            
-      }
-      printRange(idx,R,0,16);
-      //fin premier etape
-      __syncthreads();
-      turn *=2;
-      size *=2;
-      //printf("fin premiere etape\n");
-      while(size <= lenR){
-         if( idx < lenR/(turn*2)){
-
-            printf("%d -> mergePath(R+%d,S+%d ,%d, S+%d, %d,%d);\n",idx,idx*size,idx*size ,size/2,size/2+idx*size,size/2,1);
-            printRange(idx,S,4,8);
-            mergePath(R+idx*size,/*R+idx*size/2*/S+idx*size ,size/2, /*R+size/2+idx*size/2*/S+size/2+idx*size, size/2,1);
-            __syncthreads();
-            printRange(idx,R,4,8);
-         }
-         __syncthreads();
-         // mergePath(R+idx*size,/*R+idx*size/2*/S+idx*size/2 ,size/2, /*R+size/2+idx*size/2*/S+size/2+idx*size/2, size/2,turn);
-         for(int i = 0; i < size ; i++){
-            S[idx*size+i] = R[idx*size+i];
-            // T[idx*size+i] = R[idx*size+i];
-            
-         }
-        // printf("traitement de : %d\n", size);
-         turn *=2;
-         size *=2 ;
-         printRange(idx,R,0,16);
-         __syncthreads();
       }
    }
+   //optimisation ?
+   // for(int l = 2 ; l <= size; l *= 2){
+   //    for(int m = l ; m <= size ; m *=2){
+   //       printf("%d,%d -> mergePath(R+%d,S+%d ,%d, S+%d, %d,%d);\n",idx,m,idx*m,idx*m ,l/2,l/2+idx*m,l/2,1);
+   //       mergeTab(R+idx*m,S+idx*m,l/2,S+l/2+idx*m,l/2);   
+   //    }
+      
+   //    for(int i = 0; i < size ; i++){
+   //       S[idx*size+i] = R[idx*size+i];
+
+   //    }
+   //    // memcpy(S,R,menR);
+
+   // }
+  printRange(idx,R,0,lenR);
+
+
+   for(int i = 0; i < size ; i++){
+      S[idx*size+i] = R[idx*size+i];
+         
+   }
+   // memcpy(S+idx*size,R+idx*size,size);
+   //__syncthreads();
+   turn *=2;
+   size *=2;
+   idx = idx%((NB*NTPB)/turn);
+   while(size <= lenR){
+     //printf("%d,%d -> mergePath(R+%d,S+%d ,%d, S+%d, %d,%d);\n",idx,turn,idx*size,idx*size ,size/2,size/2+idx*size,size/2,1);
+      mergePath(R+idx*size, S+idx*size ,size/2, S+size/2+idx*size, size/2,1);
+      
+      for(int i = 0; i < size ; i++){
+         S[idx*size+i] = R[idx*size+i];
+         
+      }
+      // memcpy(S+idx*size,R+idx*size,size);
+
+      turn *=2;
+      idx = idx%((NB*NTPB)/turn);
+      size *=2 ;
+
+   }
+   
 }
 
 
-
+//affiche un tablrau de "len" élément(s)
 void affiche(int* A,int len){
    for (int i = 0; i < len; ++i)
    {
@@ -203,7 +242,7 @@ void affiche(int* A,int len){
    }
    puts("\b\b  ");
 }
-#define s 512
+
 
 int comp (const void * elem1, const void * elem2) 
 {
@@ -215,134 +254,64 @@ int comp (const void * elem1, const void * elem2)
 }
 
 int main(int argc,char** argv){
-   // /*int A[s] = {5,17,11,19,18,15,16,114,119,120,14,112,117,159
-   //                ,71,66,58,157,42,195,185,15,176,1124,1419
-   //                ,120,147,112,1147,19,71,646
-   //             };*/
-   // // int C[2*s] = {0};
+
+   // Initialisation des tableau
+   int *A = (int*)malloc(sizeof(int)*SIZE);
+   int* ret = (int*)malloc(sizeof(int)*SIZE);
+
    // srand(time(NULL));
-   // int* C = (int*)calloc(4*s,sizeof(int));
-   // // int A[s] = {1,2,3,4,6,10,11,13};
-   // // int B[s] = {5,7,8,9,12,14,15,16};
-   // int *A = (int*)malloc(sizeof(int)*s);
-   // // int *B = (int*)malloc(sizeof(int)*s);
+   for(int i =0 ; i < SIZE ; i++){
+      A[i] = rand()%10000+1;
+      ret[i] = 0;
 
-   // int* ret = (int*)malloc(sizeof(int)*s);
-   // for(int i =0 ; i < s ; i++){
-   //    A[i] = rand()%1000+1;
-   //    // B[i] = rand()%1000+1;
-   //    ret[i] = 0;
+   }
+   //---------------------------------
 
-   // }
+   //création des variable sur le GPU
+   int *aGPU,*bGPU;
+   testCUDA(cudaMalloc(&aGPU,SIZE*sizeof(int)));
+   testCUDA(cudaMemcpy(aGPU,A,SIZE*sizeof(int),cudaMemcpyHostToDevice));
+   testCUDA(cudaMalloc(&bGPU,SIZE*sizeof(int)));
+   testCUDA(cudaMemcpy(bGPU,A,SIZE*sizeof(int),cudaMemcpyHostToDevice));
+   //---------------------------------
+   affiche(A,SIZE); // affichage du tableau non trié
+   printf("len tab : %d\n",SIZE );
 
-   // int *aGPU,*bGPU,*cGPU;
-   // cudaMalloc(&aGPU,s*sizeof(int));
-   // cudaMemcpy(aGPU,A,s*sizeof(int),cudaMemcpyHostToDevice);
+   // gestion du temps 
+   int count;
+   cudaDeviceProp prop;
+   testCUDA(cudaGetDeviceCount(&count));
+   testCUDA(cudaGetDeviceProperties(&prop, count-1));
    
-   // cudaMalloc(&bGPU,s*sizeof(int));
-   // cudaMemcpy(bGPU,A,s*sizeof(int),cudaMemcpyHostToDevice);
-   
-   // cudaMalloc(&cGPU,2*s*sizeof(int));
-   // cudaMemcpy(cGPU,A,s*sizeof(int),cudaMemcpyHostToDevice);
-   // printf("debut :\n");
-   // sort<<<NB,NTPB>>>(aGPU,bGPU,cGPU,s);
-
-   // testCUDA(cudaMemcpy(ret,aGPU,s*sizeof(int),cudaMemcpyDeviceToHost));
-   // affiche(ret,s);
+   float TimerAddOne;                        // GPU timer instructions
+   cudaEvent_t start, stop;                  // GPU timer instructions
+   testCUDA(cudaEventCreate(&start));           // GPU timer instructions
+   testCUDA(cudaEventCreate(&stop));            // GPU timer instructions
+   testCUDA(cudaEventRecord(start,0));          // GPU timer instructions
+   //-----------------
 
 
-
-   // qsort(A,s,sizeof(int),comp);
-
-   // qsort(B,s,sizeof(int),comp);
-   int t[16] = {7,5,1,4,9,2,7,10,11,20,3,8,12,30,17,18};
-   affiche(t,16);
-   int *tGPU,*uGPU,*vGPU;
-   int *rettest = (int*)malloc(sizeof(int)*16);
-   cudaMalloc(&tGPU,16*sizeof(int));
-   cudaMemcpy(tGPU,t,16*sizeof(int),cudaMemcpyHostToDevice);
-   cudaMalloc(&uGPU,16*sizeof(int));
-   cudaMemcpy(uGPU,t,16*sizeof(int),cudaMemcpyHostToDevice);
-   cudaMalloc(&vGPU,16*sizeof(int));
-   cudaMemcpy(vGPU,t,16*sizeof(int),cudaMemcpyHostToDevice);
-   
-
-   sort<<<NB,NTPB>>>(tGPU,uGPU,vGPU,16);
+   // lancement de  l'algorithme de tri
+   sort<<<NB,NTPB>>>(aGPU,bGPU,SIZE);
 
 
-   // for(int i = 0 ; i < 4; i++){
-   //    mergePath<<<NB,NTPB>>>(tGPU+i*4,uGPU+i*4,2,vGPU+2+i*4,2,2); 
-   //    cudaMemcpy(uGPU,tGPU,16*sizeof(int),cudaMemcpyDeviceToDevice);
-   //    cudaMemcpy(vGPU,tGPU,16*sizeof(int),cudaMemcpyDeviceToDevice); 
-   //    testCUDA(cudaMemcpy(rettest,tGPU,16*sizeof(int),cudaMemcpyDeviceToHost));
-   //    affiche(rettest,16);
-   // }
-   //   for(int i = 0 ; i < 2; i++){
-   //    mergePath<<<NB,NTPB>>>(tGPU+i*8,uGPU+i*8,4,vGPU+4+i*8,4,4); 
-   //    cudaMemcpy(uGPU,tGPU,16*sizeof(int),cudaMemcpyDeviceToDevice);
-   //    cudaMemcpy(vGPU,tGPU,16*sizeof(int),cudaMemcpyDeviceToDevice); 
-   //    testCUDA(cudaMemcpy(rettest,tGPU,16*sizeof(int),cudaMemcpyDeviceToHost));
-   //    affiche(rettest,16);
-   // }
-   
-   
-   testCUDA(cudaMemcpy(rettest,tGPU,16*sizeof(int),cudaMemcpyDeviceToHost));
-   affiche(rettest,16);
+   // récupération du temps mis par le GPU
+   testCUDA(cudaEventRecord(stop,0));           // GPU timer instructions
+   testCUDA(cudaEventSynchronize(stop));        // GPU timer instructions
+   testCUDA(cudaEventElapsedTime(&TimerAddOne,     // GPU timer instructions
+          start, stop));                     // GPU timer instructions
+
+
+   // récupération du talbeau sur le CPU
+   testCUDA(cudaMemcpy(ret,aGPU,SIZE*sizeof(int),cudaMemcpyDeviceToHost));
+   printf("\n\n");
+   affiche(ret,SIZE);
+   printf("GPU Timer for the addition on the GPU of vectors: %f ms\n", 
+         TimerAddOne);
+
+   // libération de la memoire
+   free(A);free(ret);
+   testCUDA(cudaFree(aGPU));
+   testCUDA(cudaFree(bGPU));
 }
 
-
-// __global__ void merge(int* A,int lenTab){
-//    int idx = threadIdx.x + blockIdx.x * blockDim.x;
-//    int bidx = threadIdx.x;
-//    int turn = 1;
-//    int end = 0;
-//    int size = lenTab/(NB*NTPB);
-//    if(size < 2){
-//       size = 2;
-//    }
-//    __shared__ int *sT;
-//    //premier tri de size element
-
-//    //tri A[idx*size : (idx+1)*size-1]
-//    for (int i = 0; i < size; i++)
-//    {
-//       sT[bidx+i] = A[idx+i];
-
-//    }
-//    for (int j = 0; j < size; j++)
-//    {
-//       for (int k = j; k < size; k++)
-//       {
-//          if( sT[j] > sT[k]){
-//             int tmp = sT[j];
-//             sT[j] = sT[k];
-//             sT[k] = sT[j];
-//          }
-//       }
-//    }
-//    turn++;
-//    size *=2;
-//    for (int i = 0; i < size; i++)
-//    {
-//       // sT[bidx+i] = A[idx+i];
-//       A[idx] = sT[bidx];
-//    }
-//    // while(!end){
-
-//    // }
-
-
-//    //suite 
-
-
-// }
-
-
-// void mergepath(int* C,int *A,int lenA,int* B, int lenB,int nbT){
-//    int Adiag[nbT+1] = {0}
-//    int Bdiag[nbT+1] = {0}
-
-//    Adiag[nbT] = lenA
-//    Bdiag[nbT] = lenB
-//    for( int i = 0; )
-// }
